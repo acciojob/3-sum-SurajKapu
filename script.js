396541f0e3d1884bb6534d429f3c4399ee22d98e
SurajKapu <98162452+SurajKapu@users.noreply.github.com>
2023-01-16 15:10:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function threeSum(arr, target) {
-  //your code here
+	//your code here
+	let min_diff;
+	let ans;
+	for (let i = 0; i < arr.length; i++) {
+		let left = i + 1, right = arr.length - 1
+		while (left < right) {
+			let sum = arr[i] + arr[left] + arr[right]
+			let diff = (sum >= target)	? sum - target : target - sum
+			if(diff < min_diff) {
+				min_diff = diff
+				ans = sum			
+			}	
+			left++
+			right--
+		}
+	}
+	return ans
 }
 
 module.exports = threeSum;
