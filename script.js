fa237620b60f1d62deb3a922a08f9e430be584e8
SurajKapu <98162452+SurajKapu@users.noreply.github.com>
2023-01-16 15:26:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 function threeSum(arr, target) {
 	//your code here
-	let min_diff;
-	let ans;
+	arr.sort()
+	let min_diff = Number.MAX_VALUE;
+	let ans = 0;
+	let found_target = false
 	for (let i = 0; i < arr.length; i++) {
+	    if(found_target) 
+	        break;
 		let left = i + 1, right = arr.length - 1
 		while (left < right) {
 			let sum = arr[i] + arr[left] + arr[right]
-			let diff = (sum >= target)	? sum - target : target - sum
-			if(diff < min_diff) {
+			let diff = (sum >= target)	? (sum - target) : (target - sum)
+			if(diff == 0) {
+			    found_target = true
+			    break;
+			}
+			else if(diff < min_diff) {
 				min_diff = diff
-				ans = sum			
+				ans = sum
 			}	
-			left++
-			right--
+			(sum > target) ? right-- : left++
 		}
 	}
 	return ans
